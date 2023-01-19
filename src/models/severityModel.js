8ac7ffd5bb648d854dff925024d5ac4e32f25b65
jyotiranjan1997 <manomohanty281@gmail.com>
2023-01-19 15:28:28 +0530

    update correction

--- src/models/severityModel.js
+++ src/models/severityModel.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const severitySchema = mongoose.Schema({
+  user_id: { type: mongoose.ObjectId, require: true },
   name: {
     type: String,
     require: true,
   },
   value: { type: String, require: true },
-  user: { type: mongoose.isObjectIdOrHexString, require: true },
 });
 
 const Severity = mongoose.model("severity", severitySchema);
